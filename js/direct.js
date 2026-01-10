5d247c7d5a0d04e41ac179583c7ff0df6e189c22
masjidcentralokayama <ibadurrahmanislamiccenter@gmail.com>
2026-01-10 14:22:26 +0900

    Update direct.js

--- js/direct.js
+++ js/direct.js
@@ -1,6 +1,6 @@
 // 🔥 TAMBAHKAN SCRIPT URL ANDA DI SINI
 // Ganti dengan URL Google Apps Script Anda
-const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbwC1NSbdAcIAAA2r5K6P16KA77888I73JWf92TgbzhgtgRCam50n76leWPkSycIBSTA/exec";
+const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbx8tFxFZtUsYc9LvyhBFTEhN990fZ30GhwXpDuQE7fejHPWq8YDNqyNfW8_Km_becnx/exec";
 
 // ===============================
 // LANGKAH 1 — VALIDASI ID WAJIB ADA
